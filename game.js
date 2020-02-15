44c0e587c0bbed7e20ab12274df09991b37a7398
Ian <ianw3214@gmail.com>
2020-02-15 15:57:12 -0500

    player arm rotation

--- game.js
+++ game.js
@@ -256,6 +256,8 @@ function start_game() {
         self.launch_offset = 25.0;
 
         self.arm_sprite = agent_group.create(0, 0, 'player_arm');
+        self.arm_sprite.anchor.x = 0.05;
+        self.arm_sprite.anchor.y = 0.5;
 
         self.on_pickup_collision = function (player, pickup) {
             self.add_weapon(pickup.weapon_type, pickup.weapon_type.pickup_ammo);
@@ -270,7 +272,9 @@ function start_game() {
             var x = input_manager.mouse_x() - self.x
             var y = input_manager.mouse_y() - self.y
             var angle = xy_to_angle(x, y)
-            self.angle = angular_lerp(self.angle, deg(angle), self.angular_lerp, false)
+            self.angle = 0.0;
+            self.arm_sprite.angle = angular_lerp(self.arm_sprite.angle, deg(angle), self.angular_lerp, false);
+            // self.angle = angular_lerp(self.angle, deg(angle), self.angular_lerp, false)
         }
 
         self.custom_update = function () {
@@ -302,7 +306,7 @@ function start_game() {
 
             self.move(x, y);
             self.arm_sprite.x = self.x;
-            self.arm_sprite.y = self.y;
+            self.arm_sprite.y = self.y - 10.0;
 
             self.pickup_check();
 
